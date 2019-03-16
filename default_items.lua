31e1f63a5dad0bb086b363ee3154ab7548307b30
Baroque <33790616+Brqje@users.noreply.github.com>
2019-03-17 00:36:36 +0100

    Add files via upload

--- default_items.lua
+++ default_items.lua
@@ -246,6 +246,7 @@ EngInventory_DefaultSearchItemsINSERTTO = 2;
 EngInventory_DefaultSearchList = {
 	-- Catagory, Keywords, Tooltip Search, ItemType
 	-- if you put loc::, the end of the string will be looked up in the localization table
+	-- matching patterns in lines placed higher in this list will have precedence over matching patterns in lines placed lower in this list
 	{ "USED_PROJECTILE_SLOTS",	"USED_PROJECTILE_SLOT",		"",	"" },
 	{ "GRAY_ITEMS",			"ITEMRARITY_0",			"",	"" },
 	-- include .* Classes: to string search pattern first, attempt to avoid triggering on tooltip item set listings, possibly resulting in faulty catagorization
