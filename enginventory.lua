9b48d0b44bf76706f1e2bca133b81b2f91aa23ff
Baroque <33790616+Brqje@users.noreply.github.com>
2019-03-30 10:19:56 +0100

    Moved non-quiver and non-ammo-bag projectiles to another group.

--- enginventory.lua
+++ enginventory.lua
@@ -1,4 +1,4 @@
-ENGINVENTORY_VERSION = "20190323";
+ENGINVENTORY_VERSION = "20190330";
 --[[ Inventory replacement - By Engival of Shadowsong
 
  This mod is heavily inspired by AllInOneInventory. (basically learning how to script mods as AIOI as a base)
@@ -45,7 +45,7 @@ Version history
             Fixed bug with gametooltip changes in 1.10
             Integrated changes from the modified version of EngInventory posted on auctioneer's site
 	    Fixed error with hunter training window
- 20190323 - Baroque edit
+ 20190330 - Baroque edit
             Updated TOC for 1.12
             Extended bag slot support from 109 slots to 128 slots (16-slot main bag & 4x 28-slot bag)
             Modified OpenAllBags hook to allow Blizzard "Open All Bags" keybind to actually toggle, instead of just open bags (similar to default UI behaviour)
@@ -959,9 +959,8 @@ function EngInventory_SetDefaultValues(re)
 	EI_SetDefault("putinslot--CUSTOM_05", 15, 1+re, EngInventory_NumericRange, 1, ENGINVENTORY_MAX_BARS);
 	EI_SetDefault("putinslot--CUSTOM_06", 15, 1+re, EngInventory_NumericRange, 1, ENGINVENTORY_MAX_BARS);
 
-	EI_SetDefault("putinslot--EMPTY_PROJECTILE_SLOTS", 14, 1+re, EngInventory_NumericRange, 1, ENGINVENTORY_MAX_BARS);
-	EI_SetDefault("putinslot--USED_PROJECTILE_SLOTS", 14, 1+re, EngInventory_NumericRange, 1, ENGINVENTORY_MAX_BARS);
-	EI_SetDefault("putinslot--PROJECTILE", 14, 1+re, EngInventory_NumericRange, 1, ENGINVENTORY_MAX_BARS);          -- arrows and bullets that AREN'T in your shot bags	
+	EI_SetDefault("putinslot--EMPTY_PROJECTILE_SLOTS", 14, 1+re, EngInventory_NumericRange, 1, ENGINVENTORY_MAX_BARS); -- quiver and ammo bag empty slots
+	EI_SetDefault("putinslot--USED_PROJECTILE_SLOTS", 14, 1+re, EngInventory_NumericRange, 1, ENGINVENTORY_MAX_BARS); -- quiver and ammo bag used slots
 	EI_SetDefault("putinslot--SOULSHARDS", 14, 1+re, EngInventory_NumericRange, 1, ENGINVENTORY_MAX_BARS);
 	
 	EI_SetDefault("putinslot--EMPTY_SLOTS", 13, 1+re, EngInventory_NumericRange, 1, ENGINVENTORY_MAX_BARS);         -- Empty slots go in this bar
@@ -1075,6 +1074,7 @@ function EngInventory_SetDefaultValues(re)
 	EI_SetDefault("putinslot--FOOD", 3, 1+re, EngInventory_NumericRange, 1, ENGINVENTORY_MAX_BARS);
 
 	EI_SetDefault("putinslot--CLASS_ITEMS1", 2, 1+re, EngInventory_NumericRange, 1, ENGINVENTORY_MAX_BARS);
+	EI_SetDefault("putinslot--PROJECTILE", 2, 1+re, EngInventory_NumericRange, 1, ENGINVENTORY_MAX_BARS); -- arrows and bullets that AREN'T in your quiver and/or ammo bag
 
 	EI_SetDefault("putinslot--CLASS_ITEMS2", 1, 1+re, EngInventory_NumericRange, 1, ENGINVENTORY_MAX_BARS);
 	EI_SetDefault("putinslot--HEARTH", 1, 1+re, EngInventory_NumericRange, 1, ENGINVENTORY_MAX_BARS);
